0dbb0e5032fc3c64628b6e311116f54e3350b0cd
James Cleveland <jamescleveland@gmail.com>
2009-11-13 15:34:42 +0000

    Added noncore support to autoloader.

--- sample/_wwwroot/index.php
+++ sample/_wwwroot/index.php
@@ -17,6 +17,7 @@ define( "BASE_HREF", preg_replace( "/(.*?)\/index.php/", "$1", $_SERVER[ 'PHP_SE
 define( "CONFIG_PATH", SITE_PATH . DIRSEP . "config" );
 define( "CORE_PATH", SITE_PATH . DIRSEP . "core" );
 define( "HOST", $_SERVER[ "HTTP_HOST" ] );
+define( "NONCORE_PATH", SITE_PATH . DIRSEP . "noncore");
 
 # If this is set to 1, searching will far faster but less det
 # -ailed. (Using mysql full text natural searching). This for
@@ -26,15 +27,22 @@ define( "QUICK_SEARCH", 0 );
 function __autoload( $class_name )
 {
 	$filename = str_replace( "_", DIRSEP, strtolower( $class_name ) ) . '.php';
-	$file = CORE_PATH . DIRSEP . $filename;
 	
-	if( !file_exists( $file ))
+	if( file_exists( CORE_PATH . DIRSEP . $filename ))
 	{
-		die( "Could not find " . $file . "!\n" );
+		include ( CORE_PATH . DIRSEP . $filename );
+	}
+	else if( file_exists( NONCORE_PATH . DIRSEP . $filename ) )
+	{
+		include ( NONCORE_PATH . DIRSEP . $filename );		
+	}
+	else
+	{
+		die( "Could not find " . CORE_PATH . DIRSEP . $filename
+			. " or " . NONCORE_PATH . DIRSEP . $filename . "!\n" );
 		return false;
 	}
 
-	include ( $file );
 }
 
 # System happening
