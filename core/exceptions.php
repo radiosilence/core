d22fef80e13f4bae59918239ecd372578ef8494d
James Cleveland <jamescleveland@gmail.com>
2010-09-22 04:45:45 +0100

    made it display the error properly

--- core/exceptions.php
+++ core/exceptions.php
@@ -2,17 +2,16 @@
 
 namespace Core;
 
-class HTTPError extends Exception
-{
+class HTTPError extends \Exception {
     public $error_codes = array(
         404 => "HTTP/1.0 404 Not Found"
     );
-    public function __construct($code,$url){
+    public function __construct($code,$url=False){
         if(!isset($this->error_codes[$code])){
             throw new Exception("HTTP error with unknown error code.");
         }
         header($error_codes[$code]);
-        printf("Encoundered error %s Whilst trying to serve page: %s", $error_codes[$code], $url);
+        printf("Encountered error %s whilst trying to serve page: %s", $this->error_codes[$code], !empty($url) ? $url : 'unknown');
         die();
     }
 }
