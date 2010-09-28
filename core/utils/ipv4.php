aaf53873dec554e2a311453ac67077748b087682
James Cleveland <jamescleveland@gmail.com>
2010-09-28 02:21:45 +0100

    added utils dir and ipv4 getter script

---
+++ core/utils/ipv4.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the core framework.
+ *
+ * (c) James Cleveland <jamescleveland@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * Special thanks to the people at http://is.gd/fwvZW
+ */
+
+namespace Core\Utils;
+
+class IPV4 {
+    /**
+     * Retrieves the best guess of the client's actual IP address.
+     * Takes into account numerous HTTP proxy headers due to variations
+     * in how different ISPs handle IP addresses in headers between hops.
+     */
+    public static function get() {
+        // check for shared internet/ISP IP
+        if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        }
+
+        // check for IPs passing through proxies
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            // check if multiple ips exist in var
+            $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            foreach ($iplist as $ip) {
+                if ($this->validate_ip($ip)) {
+                    return $ip;
+                }
+            }
+        }
+
+        if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED'])) {
+            return $_SERVER['HTTP_X_FORWARDED'];
+        }
+        if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])){
+            return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        }
+        if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_FORWARDED_FOR'];
+        }
+        if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED'])) {
+            return $_SERVER['HTTP_FORWARDED'];
+        }
+
+        // return unreliable ip since all else failed
+        return $_SERVER['REMOTE_ADDR'];
+    }
+
+    /**
+     * Ensures an ip address is both a valid IP and does not fall within
+     * a private network range.
+     *
+     * @access public
+     * @param string $ip
+     */
+    private static function validate_ip($ip) {
+        if (filter_var($ip, FILTER_VALIDATE_IP, 
+            FILTER_FLAG_IPV4 | 
+            FILTER_FLAG_IPV6 |
+            FILTER_FLAG_NO_PRIV_RANGE | 
+            FILTER_FLAG_NO_RES_RANGE) === false) {
+            return false;
+        }
+        self::$ip = $ip;
+        return true;
+    }
+}
+?>
