97740cbcb715d7d8fddff60a99056e57fbb2ce24
James Cleveland <jamescleveland@gmail.com>
2010-09-28 02:22:23 +0100

    wrote a session container

---
+++ core/session/container.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the core framework.
+ *
+ * (c) James Cleveland <jamescleveland@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Core\Session;
+
+import('core.exceptions');
+import('core.utils.ipv4');
+
+class Container {
+    private $parameters = array();
+    public function __construct($parameters) {
+        $this->parameters = $parameters;
+    }
+
+    public function get_standard_session() {
+
+        import('core.session.handler');
+        import('core.session.remote_storage_pdo');
+        import('core.session.local_storage_cookie');
+
+        $sh = new \Core\Session\Handler();
+        $srp = new \Core\Session\RemoteStoragePDO();
+        $slc = new \Core\Session\LocalStorageCookie();
+
+        $srp->attach_pdo($this->parameters['pdo']);
+
+        try{
+            $sh->attach_remote_storage($srp)
+                ->attach_local_storage($slc)
+                ->attach_crypto_config()
+                ->set_remote_addr(\Core\Utils\IPV4::get())
+                ->start();
+            return $sh;
+        } catch(Core\Error $e) {
+            echo "SERIOUSLY AN ERROR";
+            // Colossal failure.
+            return False;   
+        }
+    }
+}
+
+?>
