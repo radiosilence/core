d456ce09bc7061e9f82fcb1a3be0b91cb58ca32a
James Cleveland <jamescleveland@gmail.com>
2010-09-30 04:15:51 +0100

    Added tests for whether importing works.

---
+++ tests/ImportTest.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'core.php';
+require_once 'vfsStream/vfsStream.php';
+
+import('core.session.handler');
+
+class ImportTest extends PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+        if (class_exists('vfsStream', false) === false) {
+            $this->markTestSkipped('vfsStream not installed.');
+        }
+        vfsStreamWrapper::register();
+        $root = vfsStream::newDirectory('home/test');
+        $root->getChild('test')->addChild(vfsStream::newFile('succeed.php'));
+        $root->getChild('test')->addChild(vfsStream::newFile('succeed2.php'));
+        $root->getChild('test')->addChild(vfsStream::newFile('succeed3.php'));
+        vfsStreamWrapper::setRoot($root);
+    }
+
+    /**
+     * Importing something that doesn't exist should raise an ImportError
+     *
+     * @expectedException ImportError
+     * @test
+     */
+    public function ImportError() {
+        import('non.existent.module');
+    }
+
+    /**
+     * Should not assert or do anything if file includes. Anything
+     * else shows lack of working php require_once. That would be bad.
+     *
+     * @test
+     */
+    public function ImportSingleFile() {
+        IMPORTER::add_include_path(vfsStream::url('home'));
+        import('test.succeed');
+    }
+
+    /**
+     * Should not assert or do anything if file includes. Anything
+     * else shows lack of working php require_once. That would be bad.
+     *
+     * @test
+     */
+    public function ImportMultiFile() {
+        IMPORTER::add_include_path(vfsStream::url('home'));
+        import('test.*');
+    }
+}
+
+?>
