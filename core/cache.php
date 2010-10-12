f7a3ae51b032122d58c7814c8f45b332376b4432
James Cleveland <jamescleveland@gmail.com>
2010-10-12 01:41:59 +0100

    added a simple static page cache

---
+++ core/cache.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Core;
+
+class Cache {
+    public function __construct($route=False) {
+        if(file_exists($this->get_path()) && !DEBUG) {
+            die(file_get_contents($this->get_path()));
+        }
+        else {
+            ob_start();
+            $this->dynamic = True;
+        }
+    }
+
+    public function __destruct() {
+        if($this->dynamic) {
+            $handle = fopen($this->get_path(), 'w');
+            fwrite($handle, ob_get_contents());
+            fclose($handle);
+            echo "GENERATED";
+        }
+    }
+
+    private function get_path() {
+        if(!is_dir(CACHE_PATH)) {
+            throw new CacheError('Cache dir does not exist.');
+        }
+        return CACHE_PATH . '/' . md5($route);
+    }
+}
+
+class CacheError extends Error {
+    
+}
+?>
