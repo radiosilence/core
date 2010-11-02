77f17b01f4e1b6837a85f77d94e4184312c65ffb
James Cleveland <jamescleveland@gmail.com>
2010-11-02 03:44:46 +0000

    made pdo container in right place

---
+++ core/containment/pdo.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core framework.
+ *
+ * (c) James Cleveland <jamescleveland@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Core\Containment;
+class PDOContainer extends \Core\ConfiguredContainer {
+
+    public function get_connection() {
+        $this->load_config('database');
+        $this->check_config();
+        extract($this->parameters['config_database']);
+        if(!isset($driver)) {
+            // Defaulting to MySQL for the driver as it is fairly common.
+            $driver = 'mysql';
+        }
+        if($driver == 'mysql') {
+            $pdo = new \PDO(sprintf('mysql:host=%s;dbname=%s',
+                    $host,
+                    $database),
+                $user,
+                $password
+            );
+        } else {
+            $pdo = new \PDO(sprintf('%s:host=%s;dbname=%s;user=%s;password=%s',
+                $driver,
+                $host,
+                $database,
+                $user,
+                $password
+            ));
+        }
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+
+    /**
+     * Make sure config is loaded.
+     */
+    private function check_config() {
+        if(!isset($this->parameters['config_database'])) {
+            throw new ConfigNotLoadedError();
+        }
+    }
+}
+
+?>
