5cb17c4f6f071ba087fe7b81c8f71f020ee85a3d
James Cleveland <jamescleveland@gmail.com>
2011-04-22 12:09:40 +0000

    fixed the variable to prefix _

--- templates/login_page.php
+++ templates/login_page.php
@@ -2,6 +2,6 @@
 <form method="POST" action="<?=($login_action ? $login_action : '/login')?>">
 <p><input type="text" name="username" id="login_username" placeholder="<?=($user_field ? $user_field : 'Username')?>"/></p>
 <p><input type="password" name="password" id="login_password" placeholder="Password"/></p>
-<p><?=$message?></p>
+<p><?=$_message?></p>
 <p><button id="login">Log In</button></p>
 </form>
